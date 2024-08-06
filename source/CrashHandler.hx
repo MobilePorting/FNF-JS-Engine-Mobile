0540f0afb139eeb81547771ab8a7fb2809becb2c
Lily <egzozu.be.bas@gmail.com>
2024-08-06 13:45:23 +0300

    Update CrashHandler.hx

--- source/CrashHandler.hx
+++ source/CrashHandler.hx
@@ -13,7 +13,7 @@ using flixel.util.FlxArrayUtil;
 
 /**
  * Crash Handler.
- * @author YoshiCrafter29, Ne_Eo and MAJigsaw77
+ * @author YoshiCrafter29, Ne_Eo, MAJigsaw77 and mcagabe19
  */
 
 class CrashHandler
